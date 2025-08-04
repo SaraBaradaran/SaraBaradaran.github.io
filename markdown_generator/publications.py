35ba6dda4e652abadbb14106e66fdde549cd086c
Sara Baradaran <82076251+SaraBaradaran@users.noreply.github.com>
2025-08-04 14:39:16 -0700

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -86,7 +86,7 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    md += "\n<small>citation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
     
@@ -98,7 +98,7 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\nRecommended citation: " + item.citation
+    md += "\n<small>Recommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
